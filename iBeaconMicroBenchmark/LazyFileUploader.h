0973fb4baee6e40517e9b1d9813f0759cd3f8273
Bo-Jhang <TimeStringAlpha@gmail.com>
2014-06-10 19:50:52 -0700

    first commit

---
+++ iBeaconMicroBenchmark/LazyFileUploader.h
@@ -0,0 +1,13 @@
+//
+//  LazyFileUploader.h
+//  iBeaconMicroBenchmark
+//
+//  Created by Bo Jhang Ho on 6/10/14.
+//  Copyright (c) 2014 Bo Jhang Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LazyFileUploader : NSObject
+
+@end
---
+++ iBeaconMicroBenchmark/LazyFileUploader.m
@@ -0,0 +1,13 @@
+//
+//  LazyFileUploader.m
+//  iBeaconMicroBenchmark
+//
+//  Created by Bo Jhang Ho on 6/10/14.
+//  Copyright (c) 2014 Bo Jhang Ho. All rights reserved.
+//
+
+#import "LazyFileUploader.h"
+
+@implementation LazyFileUploader
+
+@end
